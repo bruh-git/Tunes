a35725985b234f429ab061ddd18e693767c2e915
Bruna <brunac.cardososilva@gmail.com>
2022-03-11 00:09:01 -0300

    Requsito 8 em andamento

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicCard from './MusicCard';
+import getMusics from '../services/musicsAPI';
+import Loading from '../components/Loading';
 
 class Album extends Component {
+  constructor() {
+    super();
+    this.setState = {
+      musicAlbum: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props /* informações da rota via props do component */
+    const musicAlbum = await getMusics(id);
+    this.setState({
+      musicAlbum,
+      loading: false,
+    });
+  }
+
   render() {
+    const { musicAlbum, loading } = this.state;
+    const { match: { params: { id } } } = this.props
     return (
       <div data-testid="page-album">
         <Header />
+        {loading
+          ? <Loading /> : (
+            <MusicCard />
+          )}
       </div>
     );
   }
